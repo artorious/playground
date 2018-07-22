94e5e447aaad96aa30cd533d8cb855cbf08faebc
Arthur Ngondo <ngondoarthur@gmail.com>
2018-07-22 18:12:57 +0300

    skip tests

--- test_simple_calc.py
+++ test_simple_calc.py
@@ -1,5 +1,7 @@
 import simple_calc
 import unittest
+import sys
+
 
 class TestAddition(unittest.TestCase):
     """ Test cases for addition function add() """
@@ -14,10 +16,16 @@ class TestAddition(unittest.TestCase):
         result = simple_calc.add(10.5, 2)
         self.assertEqual(result, 12.5)
     
+    @unittest.skip('Skip this test')
     def test_add_strings(self):
         """ Test addition of two strings returns a concantenated string. """
         result = simple_calc.add('abc', 'def')
         self.assertEqual(result, 'abcdef')
+
+    @unittest.skipUnless(sys.platform.startswith("linux"), "requires Linux")
+    def test_adding_on_window(self):
+        result = simple_calc.add(1, 2)
+        self.assertEqual(result, 3)
 
 
 if __name__ == '__main__':
