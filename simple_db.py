913f1d7b6bd847216c5161f413e9b77916099e9a
Arthur Ngondo <ngondoarthur@gmail.com>
2018-07-22 12:53:50 +0300

    Delete DB entry(row) - given artist

--- simple_db.py
+++ simple_db.py
@@ -25,6 +25,18 @@ def create_database():
         "'Xplore Records', 'MP3')")
     # Save data to database
     conn.commit()
+
+    # insert multiple records using the more secure "?" method
+    albums = [
+        ('Exodus', 'Andy Hunter', '7/9/2002', 'Sparrow Records', 'CD'),
+        ('Until We Have Faces', 'Red', '2/1/2011', 'Essential Records', 'CD'),
+        ('The End is Where We Begin', 'Thousand Foot Krutch', '4/17/2012', 'TFKmusic', 'CD'),
+        ('The Good Life', 'Trip Lee', '4/10/2012', 'Reach Records', 'CD')
+    ]
+
+    cursor.executemany("INSERT INTO albums VALUES (?,?,?,?,?)", albums)
+    conn.commit()
+
     
 def select_all_albums(artist):
     """ Query the database for all the albums by a particular artist """
@@ -38,11 +50,25 @@ def select_all_albums(artist):
     conn.close()
     return result
 
+def delete_artist(artist):
+    """ Delete an artist from the database """
+    conn = sqlite3.connect("music_list.db")
+    cursor = conn.cursor()
+
+    sql = "DELETE FROM albums WHERE artist = ?"
+    cursor.execute(sql, [(artist)])
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
 
 if __name__ == '__main__':
     import os
     if not os.path.exists("music_list.db"):
         create_database()
     
-    print(select_all_albums('Chronixx'))
+    print(select_all_albums('Andy Hunter'))
+    delete_artist('Andy Hunter')
+    print(select_all_albums('Andy Hunter'))
 
